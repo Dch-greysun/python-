6903de6aea9ae4c99b860923fa7d25a9c5f915af
DCH <65151124+Dch-greysun@users.noreply.github.com>
2020-05-22 17:53:41 +0800

    Add files via upload

---
+++ 8-6城市名.py
@@ -0,0 +1,12 @@
+#定义函数
+def city_country(city, country):
+    location = (city, country)
+    #打印出结果
+    print (location)
+#给出实参并调用函数
+city_country('Beijing','China')
+city_country('Santiago','Chile')
+city_country('Tokyo','Japan')
+
+
+
