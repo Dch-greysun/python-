3ca13238928395082e6db6fe1a658d5967df0408
DCH <65151124+Dch-greysun@users.noreply.github.com>
2020-05-23 21:30:49 +0800

    Add files via upload

---
+++ 8-12三明治.py
@@ -0,0 +1,5 @@
+def make_sandwichs(toppings):
+    print('\nMaking a sandwich with the following toppings: ' + toppings.title())
+make_sandwichs('beef')
+make_sandwichs('chiken')
+make_sandwichs('pork')
